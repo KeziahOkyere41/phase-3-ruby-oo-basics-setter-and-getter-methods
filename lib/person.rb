edeed330b63647978ac6e5b1152fd703b3776815
KeziahOkyere41 <Nharnhayhaar208@gmail.com>
2022-08-26 16:28:42 +0000

    completed lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
 
-end
+    # setter method
+    def name=(name)
+      @name = name
+    end
+    def job=(job)
+        @job = (job)
+    end
+  
+    # getter method
+    def name
+      @name
+    end
+    def job
+        @job
+    end
+  
+  end
